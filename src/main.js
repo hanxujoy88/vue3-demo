e3ded6ef462c2d228e8b71a6f2e745b66b390298
hanxu <hanxu@acfun.tv>
2021-11-16 15:18:00 +0800

    目录优化

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
+/*
+ * @Author: hanxu03
+ * @Date: 2021-11-10 21:02:21
+ * @LastEditTime: 2021-11-16 15:16:54
+ * @LastEditors: hanxu03
+ * @Description: 
+ * @Enuma: Elish
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).use(router).use(router).mount('#app')
+createApp(App).use(router).mount('#app')
--- src/views/suspenseDemo.vue
+++ src/views/suspenseDemo.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: hanxu03
  * @Date: 2021-11-15 15:40:56
- * @LastEditTime: 2021-11-15 17:38:26
+ * @LastEditTime: 2021-11-16 15:16:21
  * @LastEditors: hanxu03
  * @Description: 
  * @Enuma: Elish
@@ -21,7 +21,7 @@
 import { defineAsyncComponent } from 'vue'
 export default {
     components: {
-        asyncComponent: defineAsyncComponent(() => import('./asyncComponent.vue'))
+        asyncComponent: defineAsyncComponent(() => import('../components/asyncComponent.vue'))
     }
 }
 </script>
